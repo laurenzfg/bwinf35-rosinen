66c6ff515b8cd3c59e9cebdee596334c602d96f6
Laurenz Grote <laurenz@laurenzgrote.de>
2017-04-09 12:40:35 +0200

    At lest something

--- src/de/laurenzgrote/bundeswettbewerb35/rosinen/Conglomerate.java
+++ src/de/laurenzgrote/bundeswettbewerb35/rosinen/Conglomerate.java
@@ -1,25 +1,56 @@
 package de.laurenzgrote.bundeswettbewerb35.rosinen;
 
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Repraesentation eines Firmenkonglemerates
  */
 public class Conglomerate {
-
     // Anzahl der Firmen im Konglomerat
     private int companyCount;
     // Adjazenzlisten über die Abhängigkeiten beim Unternehmenskauf
     private Company[] companys;
+    // Liste der vollständigen Dependencys
+    private BitSet[] connectedCompanys;
+    // Umgekerhter Dependency-Graph
+    private List<Integer>[] revAdj;
+    // DP-Array halt
+    private Set<BitSet>[] dpArray;
 
+    @SuppressWarnings("unchecked")
     public Conglomerate(Company[] companys) {
         this.companys = companys;
         companyCount = companys.length;
+
+        connectedCompanys = new BitSet[companyCount];
+        revAdj = new List[companyCount];
+        // Initialiseren von leeren ArrayLists
+        for (int i = 0; i < companyCount; i++) {
+            revAdj[i] = new ArrayList<>();
+        }
+        int maxPossibleRevenue = calculateMaxPossibleRevenue();
+        dpArray = new Set[maxPossibleRevenue];
+        // Initialisieren von leeren DP-Sets
+        for (int i = 0; i < maxPossibleRevenue; i++) {
+            dpArray[i] = new HashSet<>();
+        }
+    }
+
+    private int calculateMaxPossibleRevenue() {
+        int maxPossibleRevenue = 0;
+        for (int i = 0; i < companyCount; i++) {
+            double val = getImmediateValue(i);
+            if (val > 0.0) {
+                maxPossibleRevenue += val;
+            }
+        }
+        return maxPossibleRevenue;
     }
 
     public void addDependency (int ifYouBuyThat, int buyThat) {
         companys[ifYouBuyThat].addDependency(buyThat);
+        // Umgedrehte Dependency einfügen
+        revAdj[buyThat].add(ifYouBuyThat);
     }
 
     public boolean isDependent(int bought, int candidate) {
@@ -35,25 +66,60 @@ public class Conglomerate {
     }
 
     public double getImpliedValue (int of) {
-        // Visited-Array für die Tiefensuche im Graphen
-        boolean[] includedInPrice = new boolean[companyCount];
-        // Stack für Tiefensuche
-        Stack<Integer> s = new Stack<>();
-        s.add(of);
+        BitSet companys = connectedCompanys[of];
+        return getValue(companys);
+    }
 
-        double val = 0.0;
-
-        // Tiefensuche: GO
-        while (!s.empty()) {
-            int cur = s.pop();
-            if (!includedInPrice[cur]) {
-                val += getImmediateValue(cur);
-                includedInPrice[cur] = true;
-                s.addAll(getDependentCompanys(cur));
-            }
-        }
-
+    private int getValue(BitSet bs) {
+        int val = 0;
+        for (int i = 0; i < companyCount; i++)
+            if (bs.get(i))
+                val += getImmediateValue(i);
         return val;
+    }
+
+    public void calculateDependencyLists() {
+        for (int of = 0; of < companyCount; of++) {
+            // Bitset als VisitedArray
+            BitSet includedCompanys = new BitSet(companyCount);
+            // Stack für Tiefensuche
+            Stack<Integer> s = new Stack<>();
+            s.add(of);
+
+            // Tiefensuche: GO
+            while (!s.empty()) {
+                int cur = s.pop();
+                if (!includedCompanys.get(cur)) {
+                    includedCompanys.set(cur, true);
+                    s.addAll(getDependentCompanys(cur));
+                }
+            }
+
+            // Speichern der Liste
+            connectedCompanys[of] = includedCompanys;
+        }
+    }
+
+    public int bestBuy() {
+        int bestCombination = 0;
+        BitSet nothingPurchased = new BitSet(companyCount);
+        dpArray[0].add(nothingPurchased);
+        for (int i = 0; i < companyCount; i++) {
+            BitSet selectedPurchase = connectedCompanys[i];
+            Set<BitSet>[] newDPArray = dpArray.clone();
+            for (int j = 0; j <= bestCombination; j++) {
+                for (BitSet bs : dpArray[j]) {
+                    BitSet combinedBS = (BitSet) bs.clone();
+                    combinedBS.or(selectedPurchase);
+                    int combinedBSValue = getValue(combinedBS);
+                    if (combinedBSValue > 0)
+                        newDPArray[combinedBSValue].add(combinedBS);
+                    bestCombination = Math.max(bestCombination, combinedBSValue);
+                }
+            }
+            dpArray = newDPArray;
+        }
+        return bestCombination;
     }
 
     /**
@@ -74,7 +140,7 @@ public class Conglomerate {
             }
             sb.append("Gesamtwert: ");
             sb.append(getImpliedValue(i));
-            sb.append("€\n");
+            sb.append("\n");
         }
 
         return sb.toString();
--- src/de/laurenzgrote/bundeswettbewerb35/rosinen/Main.java
+++ src/de/laurenzgrote/bundeswettbewerb35/rosinen/Main.java
@@ -1,6 +1,7 @@
 package de.laurenzgrote.bundeswettbewerb35.rosinen;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -10,21 +11,34 @@ public class Main {
 
     public static void main(String[] args) {
         JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory() || file.getName().toLowerCase().endsWith(".txt");
+            }
+
+            @Override
+            public String getDescription() {
+                return ".txt-Dateiein";
+            }
+
+        });
         int returnVal = fileChooser.showOpenDialog(null);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile();
             Conglomerate conglomerate = conglomerateFromFile(file);
             System.out.println(conglomerate.toString());
+            System.out.println(conglomerate.bestBuy());
         } else {
             System.err.println("Nutzer hat den Vorgang abgebrochen!");
         }
     }
 
-    @SuppressWarnings("ConstantConditions")
     public static Conglomerate conglomerateFromFile (File file) {
         Conglomerate c = null;
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
+            @SuppressWarnings("ConstantConditions")
             int companyCount = Integer.parseInt(getNextLine(br));
             Company[] companys = new Company[companyCount];
             // Firmen mit Wert
@@ -46,9 +60,11 @@ public class Main {
                 c.addDependency(ifYouBuyThat, buyThat);
                 line = getNextLine(br);
             }
+            // Konglomerat durchrechnen
+            c.calculateDependencyLists();
         } catch (IOException e) {
             System.err.println("Eingegebene Datei nicht gefunden oder sonstiger IO-Fehler!");
-        } catch (NumberFormatException|NullPointerException e) {
+        } catch (NumberFormatException e) {
             System.err.println("Eingegebene Datei entspricht nicht der Spezifikation!");
         }
         return c;
@@ -60,7 +76,7 @@ public class Main {
                 line = br.readLine();
             } while (line.charAt(0) == '#');
             return line;
-        } catch (IndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException|NullPointerException e) {
             return null;
         }
     }
